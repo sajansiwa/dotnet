bf16dd0ca2f521b98f6ed6f4ac467925fecfafd1
sajan siwa <saj.siwa@gamil.com>
2022-09-19 09:17:44 +0545

    first commit

---
+++ hello world/Program.cs
@@ -0,0 +1,18 @@
+﻿using hello_world;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace world
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            World dis = new World();
+
+            dis.Greetings();
+
+        }
+    }
+}
