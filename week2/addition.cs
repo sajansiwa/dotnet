b9847e33beac8b7de3ffc692a1abb2ce9e0dfe87
sajan siwa <saj.siwa@gamil.com>
2022-09-19 10:33:39 +0545

    commit

---
+++ week2/addition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2
+
+
+{
+    internal class addition
+    {
+        public void Add()
+        {
+            Console.WriteLine("insert a number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("insert a number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            var addition = number1 + number2;
+            Console.WriteLine($"{addition}");  
+        }
+
+        public void sub()
+        {
+            Console.WriteLine("insert a number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("insert a number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            var substraction = number1 + number2;   
+            Console.WriteLine($"{substraction}");
+        }
+
+        public void div()
+        {
+            Console.WriteLine("insert a number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("insert a number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int division = number1 / number2;
+            Console.WriteLine($"{division}");
+        }
+
+        public void mul()
+        {
+            Console.WriteLine("insert a number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("insert a number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int multiplication = number1 * number2;
+            Console.WriteLine($"{multiplication}"); 
+        }
+
+      
+    }
+}
---
+++ week2/program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2
+{
+    internal class program : addition
+    {
+        public static void Main(String[] args)
+        {
+            var dis = new addition();
+            dis.Add();
+            dis.sub();
+        }
+    }
+}
+
+
+
+
